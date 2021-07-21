4e323749b24181c9aeed986ee8b39835e2eff092
stm32p103 <stm32p103@gmail.com>
2021-07-22 02:03:15 +0900

    qr表示追加

--- angular/localstorage/localstorage/src/app/app.component.ts
+++ angular/localstorage/localstorage/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
 import { MatSliderChange } from '@angular/material/slider';
 import { JpgLoader, QrReader } from './loader';
-
+import { toDataURL } from 'qrcode';
 
 interface RowData {
   filename: string;
   code: string | null;
   url: string | null;
+  qr: string | null;
 }
 
 @Component({
@@ -21,7 +22,7 @@ export class AppComponent  {
   private readonly loader: JpgLoader = new JpgLoader();
   private readonly reader: QrReader = new QrReader();
   rows: RowData[] = [];
-  displayedColumns: string[] = ['filename', 'code', 'image'];
+  displayedColumns: string[] = ['filename', 'code', 'image', 'qr'];
 
   async onFileSelected( event: Event ) {
     const files = ( <HTMLInputElement>event.target )?.files;
@@ -34,18 +35,39 @@ export class AppComponent  {
     for( let i = 0; i < files.length; i++ ) {
       const filename = files[i].name;
       const img = await this.loader.load( await files[i].arrayBuffer() );
-      const code = await this.reader.read( img );
+      const code = await this.reader.read( img ) || '-';
       const url = URL.createObjectURL( files[i] );  // revokeObjectURLで消すこと
+      const qr = await toDataURL( code, { errorCorrectionLevel: 'H' } );
 
       const row: RowData = {
         filename: filename,
         code: code,
-        url: url
+        url: url,
+        qr: qr
       }
 
       rows.push( row );
     }
 
+
     this.rows = rows;
+  }
+}
+
+function download(data: string, filename: string, type: string) {
+  var file = new Blob([data], {type: type});
+  if (window.navigator.msSaveOrOpenBlob) // IE10+
+      window.navigator.msSaveOrOpenBlob(file, filename);
+  else { // Others
+      var a = document.createElement("a"),
+              url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);  
+      }, 0); 
   }
 }
